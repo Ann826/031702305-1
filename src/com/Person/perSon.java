9826f560c55205988bfd59197ac503a2683a3892
ann031702305 <ann@bogon>
2019-09-17 21:15:21 +0800

    直辖市判断

--- src/com/Person/perSon.java
+++ src/com/Person/perSon.java
@@ -27,14 +27,14 @@ public class perSon {
 		// TODO Auto-generated method stub
 	       String[] strArray=new String[20000];
 	        int i=0;
-			InputStream is = new FileInputStream(args[0]);
+			InputStream is = new FileInputStream("/Users/ann/eclipse-workspace/FindPerson/src/com/Person/Homework1In.txt");
 	        String line; // 用来保存每行读取的内容
 	        BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"));
 	        line = reader.readLine(); // 读取第一行
 	        strArray[i]=line;
 	        i++;
 
-			FileOutputStream os = new FileOutputStream(args[1]);
+			FileOutputStream os = new FileOutputStream("/Users/ann/eclipse-workspace/FindPerson/src/com/Person/Homework1Out.txt");
 	        //String line; // 用来保存每行读取的内容
 	        OutputStreamWriter writer = new OutputStreamWriter(os, "UTF-8");
 	        
